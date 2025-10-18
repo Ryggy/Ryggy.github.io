a11373019233065a0a8308c1fd7f31c9f9beaa53
Ryan W <>
2025-10-18 20:02:43 +1000

    added video highlight reel

--- js/script.js
+++ js/script.js
@@ -22,8 +22,17 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
+  
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+
+  // Pause all videos before changing slides
+  let videos = document.querySelectorAll(".mySlides video");
+  videos.forEach(video => {
+    video.pause();
+    video.currentTime = 0;
+  });
+  
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -33,4 +42,10 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
+
+  // Play the video if there is one in the active slide
+  let activeVideo = slides[slideIndex - 1].querySelector("video");
+  if (activeVideo) {
+    activeVideo.play();
+  }
 } 
